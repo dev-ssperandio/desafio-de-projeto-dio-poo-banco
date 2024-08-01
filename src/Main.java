956f8e3b8b6821be5fcccd5e14a4e0a9d9ac0425
ssperandio <dev.ssperandio@gmail.com>
2024-08-01 16:38:55 -0300

    Desafio de Projeto: Criando um Banco Digital com Java e Orientação a Objetos

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Cliente sidney = new Cliente();
+        sidney.setNome("Sidney");
+
+        Conta corrente = new ContaCorrente(sidney);
+        corrente.depositar(100);
+
+        Conta poupanca = new ContaPoupanca(sidney);
+        corrente.transferir(100, poupanca);
+
+        corrente.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+    }
+}
